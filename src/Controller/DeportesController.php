628b077e8dd8594f500be23f39ee4877ddb07558
saica <david.lopezgaleote@gmail.com>
2019-02-04 09:37:14 +0100

    nuevas rutas

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -1,8 +1,19 @@
 <?php 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class DeportesController{
+    /**
+     * @Route("/deportes")
+     */
     public function inicio(){
-        return new Response('Mi primera página en Symfony!');
+        return new Response('Mi página de deportes!');
+    }
+    /**
+     * @Route("/deportes/primer-articulo")
+     */
+    public function mostrar(){
+        return new Response('Mi primera ruta Primer Artículo!');
     }
 }
